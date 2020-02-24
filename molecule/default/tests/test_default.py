c1cf2627142b727f6f5f997548cafcc9308ef43f
Thomas Boerger <thomas@webhippie.de>
2020-02-24 10:23:54 +0100

    Add more common test naming

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -7,18 +7,18 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts('all')
 
 
-def test_docker_is_installed(host):
+def test_is_installed(host):
     pkg = host.package("docker-ce")
     assert pkg.is_installed
 
 
-def test_docker_running_and_enabled(host):
+def test_running_and_enabled(host):
     svc = host.service("docker")
     assert svc.is_running
     assert svc.is_enabled
 
 
-def test_docker_config(host):
+def test_config_values(host):
     info = host.run("docker info | tr -d ' '").stdout
     assert "LiveRestoreEnabled:true" in info
     assert "LoggingDriver:json-file" in info
